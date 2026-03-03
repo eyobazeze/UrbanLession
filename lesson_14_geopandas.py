63c694eba8179a584c03d131671e86aba61edbfe
Eyob Azeze <eyobazeze9@gmail.com>
2026-03-03 12:10:40 +0300

    Spatial Intersection

--- lesson_14_geopandas.py
+++ lesson_14_geopandas.py
@@ -1,5 +1,4 @@
 import geopandas as gpd
-import matplotlib.pyplot as plt
 
 # Put the FULL path to your .shp file here
 world = gpd.read_file(r"C:\Users\sher khan\Downloads\ne_110m_admin_0_countries")
